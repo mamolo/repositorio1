db7352447dbf5aed2a89ef74d9e7685e71ead089
mamolo <oscar3880@hotmail.com>
2014-09-26 20:18:25 -0500

    Update actividad1.java

--- repositorio1/practica/actividad1.java
+++ repositorio1/practica/actividad1.java
@@ -13,5 +13,6 @@ public class actividad1 {
 public static void main (String args[]){
 
 System.out.println ("Hola este es el primer archivo subiendo en Gitnub");
+System.out.println ("Prueba de descarga");
 }
 }
