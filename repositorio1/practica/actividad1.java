3f11aa63b66c09607615b13b4f539a6640ed0dff
Sala1 <Sala1@Sala1-PC>
2014-09-26 20:13:21 -0500

    Nuestro primer archivo subido

---
+++ repositorio1/practica/actividad1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sala1
+ */
+public class actividad1 {
+    
+public static void main (String args[]){
+
+System.out.println ("Hola este es el primer archivo subiendo en Gitnub");
+}
+}
